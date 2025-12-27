3b2e8046ce7409f5928e9a59762f8ea98671e026
Hankun Lin <50829219+Reqwey@users.noreply.github.com>
2025-12-28 01:04:48 +0800

    fix: 修复错误的 aria-label

--- src/components/header/GitHubIconButton.tsx
+++ src/components/header/GitHubIconButton.tsx
@@ -5,7 +5,7 @@ export function GitHubIconButton() {
     <a
       className="size-9 rounded-full shadow-lg shadow-zinc-800/5 border border-primary bg-white/50 dark:bg-zinc-800/50 backdrop-blur flex justify-center items-center"
       type="button"
-      aria-label="Search"
+      aria-label="Repository Link"
       href={header.repoLink}
 			rel="noopener noreferrer"
 			target="_blank"
